d18244828f827872626997be930f1c040278dfc7
Erik Nordland <eriknordland@gmail,com>
2018-07-05 18:52:57 -0700

    counter commit

--- server.js
+++ server.js
@@ -9,12 +9,28 @@ app.use(express.static(__dirname + "/static"));
 // tell the express app to listen on port 8000, always put this at the end of your server.js file
 // this is the line that tells our server to use the "/static" folder for static content
 // This sets the location where express will look for the ejs views
-app.set('views', __dirname + '/views'); 
+app.set('views', __dirname + '\\views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 // Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
-app.set('view engine', 'ejs');
+// require body-parser
+var bodyParser = require('body-parser');
+// use it!
+app.use(bodyParser.urlencoded({extended: true}));
+// new code:
+var session = require('express-session');
+// original code:
+// more new code:
+app.use(session({
+  secret: 'keyboardkitteh',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
 app.listen(8000, function() {
   console.log("listening on port 8000");
-  console.log(__dirname);
+  //console.log(__dirname);
 })
 app.get("/users", function (request, response){
     // hard-coded user data
@@ -40,6 +56,29 @@ app.get("/pandas/:whichPanda", function (request,response){
         {number:3,name: "The Goose",age: "9",sleep:["bush","hole"],src:"/image/redpanda3.jpg"}
     ]
     response.render("details",{panda:pandas_array[request.params.whichPanda]});
+})
+app.get('/home',function(req,res){
+    if (req.session.count === null){
+        req.session.count = 0;
+    }
+    var myCount = req.session.count;
+    res.render("count.ejs",{Count:myCount})
+})
+app.get("/count", function(req,res){
+    req.session.count += 1;
+    res.redirect('/home');
+})
+app.post("/count",function (req,res){
+    req.session.count += 1;
+    res.redirect('/home');
+})
+app.post("/count/two",function (req,res){
+    req.session.count += 2;
+    res.redirect('/home');
+})
+app.post("/reset",function (req,res){
+    req.session.count = 0;
+    res.redirect('/count');
 })
 
 // two underscores before dirname
